f3df86378ff55acba8f2086470807b0063410fba
Usman Ghani <iamusmanbro@gmail.com>
2026-02-23 19:58:47 +0500

    ds

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import CartDrawer from './CartDrawer';
 
 const Layout = () => {
     const location = useLocation();
     const isHome = location.pathname === '/';
 
     return (
-        <div className="flex flex-col min-h-screen font-sans bg-gray-50/50 overflow-x-hidden">
+        <div className="flex flex-col min-h-screen font-sans bg-gray-50/50 overflow-x-hidden relative">
             {/* Top Notification Bar */}
             <div className="bg-[#116190] text-white text-center text-[8px] md:text-[18px] h-[21px] md:h-[35px] flex items-center justify-center font-semibold font-['Montserrat']">
                 For laboratory research use only. Not for human consumption.
@@ -17,6 +18,7 @@ const Layout = () => {
                 <Outlet />
             </main>
             <Footer />
+            <CartDrawer />
         </div>
     );
 };
